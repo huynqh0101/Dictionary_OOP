2f9c7e8444b765fedbbecac1cdc1b4042df72dc1
huynqh0101 <huynqh0101@gmail.com>
2023-11-12 23:10:05 +0700

    12/11 thay doi

--- Dictionary/src/main/java/com/example/demo/Controllers/NoticeController.java
+++ Dictionary/src/main/java/com/example/demo/Controllers/NoticeController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -21,60 +23,46 @@ public class NoticeController {
     }
 
     private void loadDataFromFile() {
-        String filePath = "C:\\Users\\Huy\\IdeaProjects\\Dictionary_OOP\\dictionaries.txt";
+        String fileName = "Data/Chudetuvung.txt"; // Đường dẫn tương đối
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             List<String> lines = new ArrayList<>();
             String line;
 
-            // Đọc tất cả các dòng từ file
             while ((line = reader.readLine()) != null) {
                 lines.add(line);
             }
 
-            // Lấy ngày hiện tại để làm seed cho số ngẫu nhiên
+            // Get seed random
             Date currentDate = new Date();
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
             String currentDateStr = dateFormat.format(currentDate);
+            String selectedWordsFileName = "Data/"+"selectedWords" + ".txt"; // Đường dẫn tương đối
 
-            // Tạo tên file selectedWords với ngày hiện tại
-            String selectedWordsFileName = "C:\\Users\\Huy\\IdeaProjects\\Dictionary_OOP\\selectedWords_" + currentDateStr + ".txt";
-
-            // Tạo một số ngẫu nhiên với seed là ngày hiện tại
             Random random = new Random(currentDate.getTime());
-
-            // Đọc danh sách từ đã được chọn trong ngày
             List<String> selectedWords = readSelectedWords(selectedWordsFileName);
-
-            // Nếu danh sách trống hoặc đã hết từ, thực hiện random lại
             if (selectedWords.isEmpty() || selectedWords.size() >= lines.size()) {
-                // Chọn một vị trí ngẫu nhiên để bắt đầu
                 int startIndex = random.nextInt(lines.size() - 15 + 1);
-
-                // Lưu lại danh sách từ đã được chọn với ngày hiện tại
                 selectedWords.clear();
                 selectedWords.add(currentDateStr);
                 for (int i = startIndex; i < startIndex + 15 && i < lines.size(); i++) {
-                    selectedWords.add(lines.get(i));
+
+                    String selectedLine = lines.get(i);
+                    if (!selectedLine.endsWith("///")) {
+                        selectedWords.add(selectedLine);
+                        markWordAsSelected(fileName, selectedLine);
+                    }
                 }
 
-                // Lưu danh sách vào file
                 writeSelectedWords(selectedWords, selectedWordsFileName);
             }
 
-            // In danh sách từ đã được chọn lên TextArea
             for (int i = 1; i < selectedWords.size(); i++) {
-                String[] parts = selectedWords.get(i).split("\t");
-                String wordE = capitalize(parts[0]);
-                explanation.appendText((i) + ". " + wordE + ":  " + parts[1] + "\n");
+                explanation.appendText((i) + ". " + selectedWords.get(i) + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private String capitalize(String str) {
-        return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 
     private List<String> readSelectedWords(String fileName) {
@@ -96,6 +84,35 @@ public class NoticeController {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             for (String word : selectedWords) {
                 writer.write(word + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void markWordAsSelected(String fileName, String selectedLine) {
+        try {
+            Path filePath = Paths.get(fileName);
+            List<String> lines = new ArrayList<>();
+            try (BufferedReader fileReader = new BufferedReader(new FileReader(filePath.toFile()))) {
+                String line;
+                while ((line = fileReader.readLine()) != null) {
+                    lines.add(line);
+                }
+            }
+
+            for (int i = 0; i < lines.size(); i++) {
+                if (lines.get(i).startsWith(selectedLine)) {
+                    lines.set(i, lines.get(i) + "///");
+                    break;
+                }
+            }
+
+            // Ghi lại nội dung đã được cập nhật vào file
+            try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(filePath.toFile()))) {
+                for (String line : lines) {
+                    fileWriter.write(line + "\n");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
