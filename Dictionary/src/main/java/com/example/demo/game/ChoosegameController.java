c24c7ca904130ad7bde2e31e85e5d9c8709aa0fe
22021202-VU-VAN-HUY <22021202@vnu.edu.vn>
2023-11-14 00:01:06 +0700

    update game

--- Dictionary/src/main/java/com/example/demo/game/ChoosegameController.java
+++ Dictionary/src/main/java/com/example/demo/game/ChoosegameController.java
@@ -1,4 +1,11 @@
 package com.example.demo.game;
 
+import javafx.scene.input.MouseEvent;
+
 public class ChoosegameController {
+    public void Choosegame1(MouseEvent mouseEvent) {
+    }
+
+    public void Choosegame2(MouseEvent mouseEvent) {
+    }
 }
---
+++ Dictionary/src/main/java/com/example/demo/game/game1/Game1Controller.java
@@ -0,0 +1,20 @@
+package com.example.demo.game.game1;
+
+import javafx.scene.input.MouseEvent;
+
+public class Game1Controller {
+    public void BacktoChoosegame(MouseEvent mouseEvent) {
+    }
+
+    public void Playgame1(MouseEvent mouseEvent) {
+    }
+
+    public void Howtoplaygame1(MouseEvent mouseEvent) {
+    }
+
+    public void Settinggame1(MouseEvent mouseEvent) {
+    }
+
+    public void Backtogame1(MouseEvent mouseEvent) {
+    }
+}
