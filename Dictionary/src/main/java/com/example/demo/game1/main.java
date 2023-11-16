1a1db68a852b41bf726400837afbad976d2554b7
22021202-VU-VAN-HUY <22021202@vnu.edu.vn>
2023-11-16 08:16:29 +0700

    clean game

--- Dictionary/src/main/java/com/example/demo/game1/main.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.game1;
-
-public class main {
-    public static void main(String[] args) {
-        game1comandline game = new game1comandline();
-        game.play();
-    }
-}
